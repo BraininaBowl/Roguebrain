5a200d508f31f3e9606b1d24e44f7b4498826efc
BraininaBowl <fransvb@gmail.com>
2025-10-03 22:24:22 +0200

    Clean up and organize

--- Source/script/helpers.js
+++ Source/script/helpers.js
@@ -1,201 +1,15 @@
-function backToCamp() {
-  player.move(map.width / 2 - 1, map.height / 2 + 2);
-  message = {};
-  message.voice = false;
-  message.text =
-    "You return to camp and settle down, taking some weight off from your tired feet.";
-  addMessage(message);
-  message = {};
-  message.voice = false;
-  message.text =
-    "Your mind is filled to the brim with today's experiences. What will you do with them?";
-  addMessage(message);
-  message = {};
-  message.choices = new Array();
-  message.choices[1] = {
-    text: "[Write a poem]",
-    action: function () {
-      message = {};
-      message.voice = false;
-      message.text = "You grab your notebook and start writing.";
-      addMessage(message);
-      restAtCamp();
-    },
-  };
-  message.choices[2] = {
-    text: "[Sketch some of the landscapes]",
-    action: function () {
-      message = {};
-      message.voice = false;
-      message.text = "You grab your sketchbook and start drawing.";
-      addMessage(message);
-      restAtCamp();
-    },
-  };
-  message.choices[3] = {
-    text: "[Do nothing, just sit and ponder]",
-    action: function () {
-      message = {};
-      message.voice = false;
-      message.text = "You sit back contently.";
-      addMessage(message);
-      restAtCamp();
-    },
-  };
-  addMessage(message);
-}
-
-function restAtCamp() {
-  document.querySelector(".maplayer").classList.add("hidden");
-  message = {};
-  message.voice = "practical";
-  message.text = "You know...";
-  addMessage(message);
-  message = {};
-  message.choices = new Array();
-  message.choices[1] = {
-    text: "[What?]",
-    action: function () {
-      message = {};
-      message.voice = "practical";
-      message.text = "Today was a pretty good day.";
-      addMessage(message);
-      message = {};
-      message.voice = "rebel";
-      message.text = "Yeah.";
-      addMessage(message);
-      message = {};
-      message.voice = "cynic";
-      message.text = "You know, it actually was.";
-      addMessage(message);
-      message = {};
-      message.choices = new Array();
-      message.choices[1] = {
-        text: "Yeah",
-        action: function () {
-          message = {};
-          message.voice = "hope";
-          message.text = "And tomorrow...";
-          addMessage(message);
-          message = {};
-          message.voice = "hope";
-          message.text = "... we can have another one!";
-          addMessage(message);
-          message = {};
-          message.choices = new Array();
-          message.choices[1] = {
-            text: "[Go for it]",
-            action: function () {
-              theEnd();
-            },
-          };
-          addMessage(message);
-        },
-      };
-      message.choices[2] = {
-        text: "Not really",
-        action: function () {
-          message = {};
-          message.voice = "hope";
-          message.text = "Oh...";
-          addMessage(message);
-          message = {};
-          message.voice = "hope";
-          message.text = "But tomorrow we can try again!";
-          addMessage(message);
-          message = {};
-          message.choices = new Array();
-          message.choices[1] = {
-            text: "[Go for it]",
-            action: function () {
-              theEnd();
-            },
-          };
-          addMessage(message);
-        },
-      };
-      addMessage(message);
-    },
-  };
-  addMessage(message);
-}
-
-function goToNextStop() {
-  message = {};
-  message.choices = new Array();
-  message.choices[1] = {
-    text: "[Continue]",
-    action: function () {
-      nextStop();
-    },
-  };
-  addMessage(message);
-}
-
-function getPoiChoice(poi, dir) {
-  let choice = {
-    text: "[Go " + dir + "]",
-    action: function () {
-      player.move(poi.x, poi.y);
-      if (pois_lut[poi.type].action["a" + player.seen[poi.type]] == undefined) {
-        player.seen[poi.type] -= 1;
-      }
-      pois_lut[poi.type].action["a" + player.seen[poi.type]]();
-      player.seen[poi.type] += 1;
-
-      delete map.pois[poi.id];
-      //nextStop()
-    },
-  };
-  return choice;
-}
-
-function runEvents() {
+/* Event queue */
+function runEvents(delay = 0) {
   if (events.length > 0) {
     events[0]();
     events.shift();
-    runEvents();
+    setTimeout(function () {
+      runEvents(delay);
+    }, delay);
   }
 }
 
-function findPois() {
-  let pois_north = new Array();
-  let pois_south = new Array();
-  let pois_west = new Array();
-  let pois_east = new Array();
-  //Sort all pois relative to the player
-  for (let value of Object.values(map.pois)) {
-    let rel_x = Math.abs(player.x - value.x);
-    let rel_y = Math.abs(player.y - value.y);
-    if (rel_x > rel_y) {
-      //east or west
-      if (player.x > value.x) {
-        // west
-        pois_west[rel_x + rel_y] = value;
-      } else if (player.x < value.x) {
-        // east
-        pois_east[rel_x + rel_y] = value;
-      }
-    } else {
-      //north or south
-      if (player.y > value.y) {
-        // north
-        pois_north[rel_x + rel_y] = value;
-      } else if (player.y < value.y) {
-        // south
-        pois_south[rel_x + rel_y] = value;
-      }
-    }
-  }
-
-  return {
-    n: pois_north.find((x) => x !== undefined),
-    s: pois_south.find((x) => x !== undefined),
-    e: pois_east.find((x) => x !== undefined),
-    w: pois_west.find((x) => x !== undefined),
-  };
-}
-
+/* Log */
 function removeActiveLinks() {
   document.querySelectorAll(".active_link").forEach((item) => {
     //item.classList.remove("active_link");
@@ -204,28 +18,7 @@ function removeActiveLinks() {
   });
 }
 
-function lineOfSight(x1, y1, x2, y2, usecover = false) {
-  const points = bresenham(x1, y1, x2, y2);
-  let result = true;
-  points.forEach((item) => {
-    let square = map.readMap(item.x, item.y);
-    if (square == undefined || square.passable == false) {
-      result = false;
-    }
-    if (
-      usecover &&
-      !(item.x == x1 && item.y == y1) &&
-      !(item.x == x2 && item.y == y2) &&
-      map.content(item.x, item.y) != false &&
-      map.content(item.x, item.y) != "pass" &&
-      map.content(item.x, item.y) != "player"
-    ) {
-      result = false;
-    }
-  });
-  return result;
-}
-
+/* Data */
 function cloneTable(data) {
   "use strict";
   return JSON.parse(JSON.stringify(data));
@@ -243,11 +36,12 @@ function getDir(x1, y1, x2, y2) {
   }
 }
 
+/* Text manipulation */
 function capitalize(sentence) {
   return sentence.substring(0, 1).toUpperCase() + sentence.substring(1);
 }
 
-function numtotext(num) {
+function numToText(num) {
   let numbers = [
     "zero",
     "one",
@@ -293,11 +87,10 @@ function suffix(number) {
   return number + "th";
 }
 
+/* Maths */
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
-
-const removeDuplicates = (arr) => [...new Set(arr)];
 
 function limit(val, min, max) {
   if (val < min) {
@@ -307,10 +100,6 @@ function limit(val, min, max) {
   } else {
     return val;
   }
-}
-
-function execute(code) {
-  setTimeout(code, 0);
 }
 
 function wrap(val, min, max) {
@@ -323,6 +112,7 @@ function wrap(val, min, max) {
   return val;
 }
 
+/* Algorithms */
 function dijkstra(source_x, source_y, dest_x, dest_y) {
   let queue = [];
   let dijkstramap = {};
@@ -527,4 +317,26 @@ function bresenham(x0, y0, x1, y1) {
   }
 
   return points;
+}
+
+function lineOfSight(x1, y1, x2, y2, usecover = false) {
+  const points = bresenham(x1, y1, x2, y2);
+  let result = true;
+  points.forEach((item) => {
+    let square = map.readMap(item.x, item.y);
+    if (square == undefined || square.passable == false) {
+      result = false;
+    }
+    if (
+      usecover &&
+      !(item.x == x1 && item.y == y1) &&
+      !(item.x == x2 && item.y == y2) &&
+      map.content(item.x, item.y) != false &&
+      map.content(item.x, item.y) != "pass" &&
+      map.content(item.x, item.y) != "player"
+    ) {
+      result = false;
+    }
+  });
+  return result;
 }
